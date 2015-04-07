08d570177112a9582f096f3b51e94daed2567adf
Rowan Manning <info@rowanmanning.com>
2015-04-07 14:27:39 +0100

    Tabs, not spaces

--- test/unit/lib/try-require.js
+++ test/unit/lib/try-require.js
@@ -6,34 +6,34 @@ var assert = require('proclaim');
 var mockery = require('mockery');
 
 describe('lib/try-require', function () {
-    var tryRequire, fooModule;
+	var tryRequire, fooModule;
 
-    beforeEach(function () {
-        fooModule = {};
-        mockery.registerMock('foo', fooModule);
-        tryRequire = require('../../../lib/try-require');
-    });
+	beforeEach(function () {
+		fooModule = {};
+		mockery.registerMock('foo', fooModule);
+		tryRequire = require('../../../lib/try-require');
+	});
 
-    it('should be a function', function () {
-        assert.isFunction(tryRequire);
-    });
+	it('should be a function', function () {
+		assert.isFunction(tryRequire);
+	});
 
-    describe('tryRequire()', function () {
+	describe('tryRequire()', function () {
 
-        it('should return the required module if it exists', function () {
-            assert.strictEqual(tryRequire('foo'), fooModule);
-        });
+		it('should return the required module if it exists', function () {
+			assert.strictEqual(tryRequire('foo'), fooModule);
+		});
 
-        it('should return `undefined` if a module does not exist', function () {
-            assert.isUndefined(tryRequire('bar'));
-        });
+		it('should return `undefined` if a module does not exist', function () {
+			assert.isUndefined(tryRequire('bar'));
+		});
 
-        it('should not throw if a module does not exist', function () {
-            assert.doesNotThrow(function () {
-                tryRequire('bar');
-            });
-        });
+		it('should not throw if a module does not exist', function () {
+			assert.doesNotThrow(function () {
+				tryRequire('bar');
+			});
+		});
 
-    });
+	});
 
 });
